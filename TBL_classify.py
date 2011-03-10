fe032c380130e42f8c1bd6db0c52576dc73ea59f
nemo <nemo@dreammaster.local>
2011-03-10 13:37:03 -0800

    commit

--- TBL_classify.py
+++ TBL_classify.py
@@ -6,16 +6,36 @@
 #imports
 import sys
 
-#arguments
+
+
+
+def get_model(model_filename):
+	model_file = open(model_filename,'r')
+	
+
+def get_vectors(test_data_filename):
+	vectors = {}
+	labels = set()
+	
+	test_file = open(test_data_filename,'r')
+	for instance in test_file.readlines():
+		instance = instance.split()
+		instance_name = instance[0]
+		label = instance[1]
+		features = line_array[2::2] 
+		values = line_array[3::2]
+		vectors[instance_name] = {}
+		vectors[instance_name]["_label_"] = label
+		labels.add(label)
+		for (f, v) in zip(features, values):
+			vectors[instance_name][f] = v
+	return [vectors, labels]
+	
 #The command line is: TBL classify.sh test_data model_file sys_output N
 try: #do casting here, too
-	train_data_filename = sys.argv[1]
-	model_file = sys.argv[2]
+	test_data_filename = sys.argv[1]
+	model_filename = sys.argv[2]
 	sys_output = sys.argv[3]
 	N = sys.argv[4]
 except Exception:
-	print "requires arguments: train_data model_file min_gain"
-
-
-
-
+	print "requires arguments: train_data model_file sys_output N"
