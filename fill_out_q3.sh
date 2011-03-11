a82d9885e6778b5e4138559bf202af18be142879
nemo <nemo@dreammaster.local>
2011-03-11 12:08:11 -0800

    done, tested, low numbers

--- fill_out_q3.sh
+++ fill_out_q3.sh
@@ -4,6 +4,12 @@ ns=( 1 5 10 20 50 100 150 200 250 )
  
 for n in "${ns[@]}" 
 do
+	
+	echo "Train accuracy: N=$n"
+	test_acc_array=`./TBL_classify.sh  examples/train2.txt model_file sys_output $n`
+	echo "${test_acc_array[0]}"
+	echo -e "\n"
+	
 	echo "Test accuracy: N=$n"
 	test_acc_array=`./TBL_classify.sh  examples/test2.txt model_file sys_output $n`
 	echo "${test_acc_array[0]}"
