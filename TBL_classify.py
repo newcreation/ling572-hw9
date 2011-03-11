52351605b6a7996372c0b0655679ce161bd7d599
nemo <nemo@dreammaster.local>
2011-03-11 11:59:23 -0800

    done, tested, low numbers

--- TBL_classify.py
+++ TBL_classify.py
@@ -1,8 +1,3 @@
- 
-# model file has the default classname (i.e., the first class in the training data) in the first line, 
-# followed by a list of transformations (one transformation per line). 
-# The transformation line has the format “featName from_class to_class net_gain”.
-
 #imports
 import sys
 
@@ -23,13 +18,14 @@ def get_vectors(test_data_filename):
 		instance = instance.split()
 		instance_name = instance[0]
 		label = instance[1]
-		features = line_array[2::2] 
-		values = line_array[3::2]
+		features = instance[2::2] 
+		values = instance[3::2]
 		vectors[instance_name] = {}
 		vectors[instance_name]["_label_"] = label
 		labels.add(label)
 		for (f, v) in zip(features, values):
 			vectors[instance_name][f] = v
+
 	return [vectors, labels]
 
 def classify(vectors,model,N):
@@ -43,19 +39,21 @@ def classify(vectors,model,N):
 			curr_hypothesis = model[0]
 			data[instance] = {}
 			transformations = []
-			data[instance]["_truelabel_"] = vectors[instance]["label"]
-			for index in range(1,min(N,len(model)):
+			data[instance]["_truelabel_"] = vectors[instance]["_label_"]
+			syslabel = ""
+			for index in range(1,min(N,len(model))):
 				feat_name,from_class,to_class,net_gain = model[index].split()
 				if feat_name in vectors[instance]:
-					if curr_hypothesis == from_class:
+
+					if curr_hypothesis.strip() == from_class.strip():
 						transformations.append((feat_name,from_class,to_class))
 						curr_hypothesis = to_class
-			data[instance]["_syslabel_"] = to_class
+			data[instance]["_syslabel_"] = curr_hypothesis
 			data[instance]["_transformations_"] = transformations
 	return data
 	
 def print_sys(data,sys_output_filename):
-	sys_file = open(sys_output_filename, 'r')
+	sys_file = open(sys_output_filename, 'w')
 	correct = 0
 	total = 0
 	for instance in data:
@@ -65,7 +63,7 @@ def print_sys(data,sys_output_filename):
 		if truelabel==syslabel:
 			correct +=1
 		sys_file.write(instance+" ")
-		sys_file.write(+truelabel" ")
+		sys_file.write(truelabel+" ")
 		sys_file.write(syslabel+" ")
 		for transformation in data[instance]["_transformations_"]:
 			sys_file.write(transformation[0]+" "+transformation[1]+" "+transformation[2])
@@ -73,18 +71,17 @@ def print_sys(data,sys_output_filename):
 	return [correct,total]
 		
 def print_acc(correct,total):
-	print "Correct: " + correct
-	print "Total: " + total
-	print "Accuracy " + correct/total
+	print "Correct: " + str(correct)
+	print "Total: " + str(total)
+	print "Accuracy " + str(float(correct)/float(total))
 
-try:
-	test_data_filename = sys.argv[1]
-	model_filename = sys.argv[2]
-	sys_output = sys.argv[3]
-	N = int(sys.argv[4])
-except Exception:
-	print "requires arguments: train_data model_file sys_output N"
-	
+
+
+test_data_filename = sys.argv[1]
+model_filename = sys.argv[2]
+sys_output_filename = sys.argv[3]
+N = int(sys.argv[4])	
+
 vectors,labels = get_vectors(test_data_filename)
 model = get_model(model_filename)
 data = classify(vectors,model,N)
--- TBL_classify.sh
+++ TBL_classify.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-python TBL_classify.py $1 $2 $3 $4 
+python TBL_classify.py $1 $2 $3 $4
